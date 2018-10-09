87f28e0bd0ab2716a51e89cd291c2524d6d6bdbf
ghostwyd <muou1986227@163.com>
2018-10-09 23:22:31 +0800

    test

---
+++ digital_process/divide.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+//https://leetcode-cn.com/problems/divide-two-integers/description/
+int divide(int dividend, int divisor) {
+    if (dividend >=0 && divisor > 0) {
+    }
+}
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
